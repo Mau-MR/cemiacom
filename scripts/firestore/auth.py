218117349ebdd0ece44bbd08a9a9176298d9203f
Mauricio Merida <maudevops@gmail.com>
2021-03-22 18:45:24 -0600

    firestore user  insertion
    The insertion of  the new user  to  the database

--- scripts/firestore/auth.py
+++ scripts/firestore/auth.py
@@ -1,4 +1,18 @@
+from google.cloud import firestore_v1 as firestore
 
+USERS = u'Users'
+
+
+def create_user(mail, pwd, institution, phone, name, surname):
+    db = firestore.Client()
+    db.collection(USERS).add({
+        u'Mail': mail,
+        u'Password': encrypt_password(pwd),
+        u'Phone': phone,
+        u'Name': name,
+        u'Surname': surname,
+        u'Institution': institution,
+    })
 
 
 # Receives the password and encrypts it
